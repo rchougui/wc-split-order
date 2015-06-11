a275e300afa80b5cd3ee3c17068226cc397f8854
Riadh <crazy2death@gmail.com>
2015-06-11 12:41:19 +0100

    Prevent user from moving  more than the quantity in the original item line.

--- admin/js/wc-pending-split-admin.js
+++ admin/js/wc-pending-split-admin.js
@@ -28,7 +28,8 @@
 						'<tr>'+
 							'<td>'+el.thumb+'</td>'+
 							'<td>'+el.name+'</td>'+
-							'<td><input type="number" step="1" min="1" max="" autocomplete="off"  placeholder="1" value="1" size="4" class="quantity"></td>'+
+							'<td>'+el.item_cost_view+'</td>'+
+							'<td><input type="number" step="1" min="1" data-max_qty="'+parseInt(el.max_qty)+'" autocomplete="off"  placeholder="1" value="1" size="4" class="move-quantity"></td>'+
 						'</tr>'
 						);
 				});
@@ -60,17 +61,30 @@
 		$('#wc_split_button').click(function(){
 						
 			var WCSplitModal = new WCSplitBackboneModal();
-
+			
+			//retrieve a list of objects from the selected products to be moved.
 			var order_item_ids=[];
+
 			$("#order_line_items .item").has('.check-column input:checked').each(function(index, el) {
 				order_item_ids.push({
 					item_id : $(el).data('order_item_id'),
 					thumb : $(el).find('.thumb').html(),
 					name : $(el).find('.name').html(),
+					item_cost : $(el).find('.item_cost').data('sort-value'),
+					item_cost_view : $(el).find('.item_cost .view').html(),
+					max_qty : $(el).find('.quantity .view').html()
 				});
 			});
+
+			//only open modal if we have selected products.
 			if(order_item_ids.length >0) {
-			WCSplitModal.render(order_item_ids);
+				WCSplitModal.render(order_item_ids);
+				//prevent user from moving products more than their original quantity.
+				$('body').on('change', '.move-quantity', function(e){
+					if($(this).val() > $(this).data('max_qty')){
+						$(this).val($(this).data('max_qty'));
+					}
+				})
 			}
 			
 		})
