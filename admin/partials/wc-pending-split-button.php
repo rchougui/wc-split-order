99bf18f6be668ae908860c3d2e288d5eb3dcd894
Riadh <crazy2death@gmail.com>
2015-06-11 11:50:46 +0100

    Added modal template.

--- admin/partials/wc-pending-split-button.php
+++ admin/partials/wc-pending-split-button.php
@@ -11,5 +11,27 @@
  */
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
 <button type="button" class="button" id="wc_split_button" data-order_id="<?php echo $order_id;?>">Move to split order</button>
+
+<script type="text/template" id="wc-modal-split-products">
+	<div class="wc-backbone-modal">
+		<div class="wc-backbone-modal-content">
+			<section class="wc-backbone-modal-main" role="main">
+				<header class="wc-backbone-modal-header">
+					<a class="modal-close modal-close-link" href="#"><span class="close-icon"><span class="screen-reader-text">Close media panel</span></span></a>
+					<h1>Move products to split order</h1>
+				</header>
+				<article>
+					
+				</article>
+				<footer>
+					<div class="inner">
+						<button id="btn-split" class="button button-primary button-large" data-order_id="<?php echo $order_id;?>">Move</button>
+					</div>
+				</footer>
+			</section>
+		</div>
+	</div>
+	<div class="wc-backbone-modal-backdrop modal-close">&nbsp;</div>
+</script>
